b0439de34c7cb52ec3a7cc42ca32373fde095d10
kul <mukuldms7@gmail.com>
2019-09-25 21:15:30 +0530

    3

--- cohost_trioog2/mail.php
+++ cohost_trioog2/mail.php
@@ -12,4 +12,5 @@ $MESSAGE_BODY .= "Suggestions: ".nl2br($_POST["suggestions"])." \n";
 mail($ToEmail,$EmailSubject,$MESSAGE_BODY,"From:".$from);
 
 header('Location: index.html#Sucessful');
+echo "Thank You!" . " -" . "<a href='index.html'>Return Home</a>";
 ?>
